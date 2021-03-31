e18b44908db1f3a49ba884f0b90ef555f936eae6
Joe Marshall <joe.marshall@nottingham.ac.uk>
2021-03-31 17:03:42 +0100

    onedrive support

--- knocktest1.py
+++ knocktest1.py
@@ -1,8 +1,7 @@
 from sensors import accel
 from sensors import sound
 import graphs
-import asyncio,async_pyodide
-
+from time import sleep
 
 class low_pass:
     def __init__(self,alpha,gain=None):
@@ -45,44 +44,37 @@ class high_pass:
         return outVal*self.gain
 
 
-async def main_fn():
-    hp_mag= high_pass(0.1)
-    hp_snd = high_pass(0.1)
-    lp_mag= low_pass(0.5)
-    lp_snd= low_pass(0.5)
-    graphs.setStyle("Sound","blue",-1,1)        
-    graphs.setStyle("Sound_HP","green",-1,1)        
-    graphs.setStyle("Sound_LP","white",-1,1)        
-    graphs.setStyle("Accel","red",0,20)
-    graphs.setStyle("Accel_HP","yellow",-10,10)
-    while not sound.get_level() and not accel.get_magnitude():
-        print("Can't find sensors")
-        print("Click start sensors to run script")
-        await asyncio.sleep(1)
-        
-    while True:
-        mag=accel.get_magnitude()
-        snd= sound.get_level()
-        if mag:
-            mag_lowpassed=lp_mag.update(mag)
-            mag_highpassed=hp_mag.update(mag)
-            graphs.onValue("Accel",mag)
-            graphs.onValue("Accel_HP",mag_highpassed)
+hp_mag= high_pass(0.1)
+hp_snd = high_pass(0.1)
+lp_mag= low_pass(0.5)
+lp_snd= low_pass(0.5)
+graphs.set_style("Sound","blue",-1,1)        
+graphs.set_style("Sound_HP","green",-1,1)        
+graphs.set_style("Sound_LP","white",-1,1)        
+graphs.set_style("Accel","red",0,20)
+graphs.set_style("Accel_HP","yellow",-10,10)
+    
+while True:
+    mag=accel.get_magnitude()
+    snd= sound.get_level()
+    if mag:
+        mag_lowpassed=lp_mag.update(mag)
+        mag_highpassed=hp_mag.update(mag)
+        graphs.on_value("Accel",mag)
+        graphs.on_value("Accel_HP",mag_highpassed)
 
 
-            if mag_highpassed>1:
-                print("Accelerometer knock")
-        if snd:
-            snd_lowpassed=lp_snd.update(snd)
-            snd_highpassed=hp_snd.update(snd)
-            graphs.onValue("Sound",snd)        
-            graphs.onValue("Sound_LP",snd_lowpassed)        
-            graphs.onValue("Sound_HP",snd_highpassed)        
-            if snd_highpassed>0.2:
-                print("Sound knock")
-            if snd_highpassed<-0.2:
-                print("Sound knock off")
-        await asyncio.sleep(.01);
+        if mag_highpassed>1:
+            print("Accelerometer knock")
+    if snd:
+        snd_lowpassed=lp_snd.update(snd)
+        snd_highpassed=hp_snd.update(snd)
+        graphs.on_value("Sound",snd)        
+        graphs.on_value("Sound_LP",snd_lowpassed)        
+        graphs.on_value("Sound_HP",snd_highpassed)        
+        if snd_highpassed>0.2:
+            print("Sound knock")
+        if snd_highpassed<-0.2:
+            print("Sound knock off")
+    sleep(.01);
 
-loop=asyncio.get_event_loop()
-loop.set_task_to_run_until_done(main_fn())
